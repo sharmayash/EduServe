7b3005b8de1c4d14b13505aeb81a2663d8ae8840
Raghav Kaushik <raghav.kaushik611@gmail.com>
2020-02-09 19:11:47 +0530

    #8 throttling of requests

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const compression = require("compression")
 const { ApolloServer } = require("apollo-server-express")
+const rateLimit = require('express-rate-limit')
+
 require('./db/mongoose')    // DB connection initialize
 
 const resolvers = require("./graphql/resolvers/index")
 
 const app = express()
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message:
+    "Too many accounts created from this IP, please try again after 15 minutes"
+});
+
+app.use(limiter)  // apply to all requests
+
 app.use(cors())
+
 app.use(compression())
 
 const port = process.env.PORT || 4000
