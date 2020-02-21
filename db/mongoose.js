71cb8e94f134a7546084c3a06be161c481fa1142
unknown <sharmayash44ys@gmail.com>
2020-02-21 12:12:37 +0530

    #7 mongodb atlas uri addded

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(
-  process.env.MONGO_URI || "mongodb://localhost:27017/eduserve", {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-})
-  .then(() => console.log('connected'))
-  .catch(err => console.log(err.message + '\n' + err.reason))
+mongoose
+	.connect(
+		process.env.MONGO_URI ||
+			"mongodb+srv://eduserveUser:qwertyuiop@cluster0-ftn3n.mongodb.net/test?retryWrites=true&w=majority",
+		{
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false
+		}
+	)
+	.then(() => console.log("connected"))
+	.catch(err => console.log(err.message + "\n" + err.reason))
