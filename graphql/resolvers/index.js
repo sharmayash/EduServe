69f1d47d32c2cb40aacec935e540b5e28b4d3997
unknown <sharmayash44ys@gmail.com>
2020-02-29 12:03:28 +0530

    #7 FIXME: count no of colleges

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -105,6 +105,35 @@ const resolvers = {
 					createdAt,
 					updatedAt
 				}
+		},
+		getCategoryInfo: async (_, args, req) => {
+			let categoryArr = []
+
+			let tempArr = []
+
+			const allcolleges = await College.find({}, "collegename coursesOffered")
+
+			await allcolleges.map(cllg => {
+				cllg.coursesOffered.map(course => {
+					tempArr.push({
+						cllgName: cllg.collegename,
+						category: course.category
+					})
+				})
+			})
+
+			tempArr.map(x => {
+				if (!categoryArr.some(item => item.catName === x.category)) {
+					// if stream not exist in category array
+					categoryArr.push({ catName: x.category, noOfColleges: 1 })
+					// console.log(x.cllgName, x.category)
+				} else {
+					// console.log(x.cllgName, x.category, "ELSE PART")
+					// FIXME: College count krne ka logic
+				}
+			})
+
+			return categoryArr
 		}
 	},
 	Mutation: {
